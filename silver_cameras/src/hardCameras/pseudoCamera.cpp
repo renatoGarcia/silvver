6b456b27b93271e2ea835858b638a72a7adf38f4
Renato Garcia <fgar.renato@gmail.com>
2009-11-09 13:36:02 +0000

    Feitas as mudanças para a pseudoCamera funcionar com multiplas abstract camera

--- silver_cameras/src/hardCameras/pseudoCamera.cpp
+++ silver_cameras/src/hardCameras/pseudoCamera.cpp
@@ -1,6 +1,21 @@
+/* Copyright 2009 Renato Florentino Garcia <fgar.renato@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "pseudoCamera.hpp"
 
-#include <boost/thread/thread.hpp>
+#include <boost/bind.hpp>
 
 namespace bfs = boost::filesystem;
 namespace bpt = boost::posix_time;
@@ -10,10 +25,20 @@ PseudoCamera::PseudoCamera(const scene::PseudoCamera& config)
   ,path(config.imagesPath)
   ,frameRate(config.frameRate)
   ,delay((static_cast<long>((1.0 / this->frameRate) * 1.0e3)))
+  ,currentFrame(NULL)
+  ,frameBuffer({NULL, NULL})
 {}
 
 PseudoCamera::~PseudoCamera()
-{}
+{
+  for (int i=0; i<2; ++i)
+  {
+    if (this->frameBuffer[i] != NULL)
+    {
+      cvReleaseImage(&this->frameBuffer[i]);
+    }
+  }
+}
 
 void
 PseudoCamera::initialize()
@@ -25,6 +50,71 @@ PseudoCamera::initialize()
   }
 
   this->dirIterator = bfs::directory_iterator(this->path);
+
+  this->grabFrameThread.reset(new boost::thread(boost::bind<void>
+                                                (&PseudoCamera::runCapturer,
+                                                 this)));
+}
+
+void
+PseudoCamera::runCapturer()
+{
+  int frameIdx = 0;
+  bool imageLoaded = false;
+
+  while (true)
+  {
+    boost::this_thread::interruption_point();
+
+    // Release the buffer
+    if (this->frameBuffer[frameIdx] != NULL)
+    {
+      cvReleaseImage(&this->frameBuffer[frameIdx]);
+    }
+
+    imageLoaded = false;
+    do
+    {
+      if(this->dirIterator == this->endIterator)
+      {
+        // boost::this_thread::sleep(this->delay);
+        // throw capture_image_error("PseudoCamera: all images already read");
+        // continue;
+        return;
+      }
+
+      if(bfs::is_directory(dirIterator->status()))
+      {
+        this->dirIterator++;
+        continue;
+      }
+
+      this->frameBuffer[frameIdx] =
+        cvLoadImage(dirIterator->path().file_string().c_str(),
+                    CV_LOAD_IMAGE_COLOR);
+      if(this->frameBuffer[frameIdx] == NULL)
+      {
+        this->dirIterator++;
+        continue;
+        // throw capture_image_error("Error loading image: " +
+        //                           dirIterator->path().file_string());
+      }
+
+      imageLoaded = true;
+      this->dirIterator++;
+    }while(!imageLoaded);
+
+    this->bufferAccess.lock();
+    this->currentFrame = this->frameBuffer[frameIdx];
+    this->bufferAccess.unlock();
+
+    std::fill(this->unreadImage.begin(), this->unreadImage.end(), true);
+    this->unreadFrameCondition.notify_all();
+
+    frameIdx = (frameIdx+1) % 2;
+
+    boost::this_thread::sleep(this->delay);
+  }
 }
 
 void
@@ -34,35 +124,15 @@ PseudoCamera::saveFrame()
 void
 PseudoCamera::captureFrame(IplImage** iplImage, unsigned clientUid)
 {
-  boost::mutex::scoped_lock lock(this->mutexCaptureFrame);
+  boost::shared_lock<boost::shared_mutex> lock(this->bufferAccess);
 
-  bool imageLoaded = false;
-  while(!imageLoaded)
+  while (!this->unreadImage.at(clientUid))
   {
-    if(this->dirIterator == this->endIterator)
-    {
-      boost::this_thread::sleep(this->delay);
-      throw capture_image_error("PseudoCamera: all images already read");
-    }
-
-    if(bfs::is_directory(dirIterator->status()))
-    {
-      this->dirIterator++;
-      continue;
-    }
-
-    cvReleaseImage(iplImage);
-    *iplImage = cvLoadImage(dirIterator->path().file_string().c_str(),
-                            CV_LOAD_IMAGE_COLOR);
-    if(*iplImage == NULL)
-    {
-      this->dirIterator++;
-      throw capture_image_error("Error loading image: " +
-                                dirIterator->path().file_string());
-    }
-    imageLoaded = true;
-    this->dirIterator++;
+    this->unreadFrameCondition.wait(lock);
   }
 
-  boost::this_thread::sleep(this->delay);
+  cvReleaseImage(iplImage);
+  *iplImage = cvCloneImage(this->currentFrame);
+
+  this->unreadImage.at(clientUid) = false;
 }
--- silver_cameras/src/hardCameras/pseudoCamera.hpp
+++ silver_cameras/src/hardCameras/pseudoCamera.hpp
@@ -1,3 +1,18 @@
+/* Copyright 2009 Renato Florentino Garcia <fgar.renato@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef PSEUDO_CAMERA_HPP
 #define PSEUDO_CAMERA_HPP
 
@@ -6,7 +21,11 @@
 #include <string>
 
 #include <boost/filesystem.hpp>
-#include <boost/thread/mutex.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/thread/condition.hpp>
+#include <boost/thread/shared_mutex.hpp>
+#include <boost/thread/thread.hpp>
+// #include <boost/thread/mutex.hpp>
 
 class PseudoCamera : public HardCamera
 {
@@ -27,6 +46,8 @@ private:
 
   static const unsigned BITS_PER_PIXEL = 8;
 
+  void runCapturer();
+
   const boost::filesystem::path path;
 
   const float frameRate;
@@ -36,7 +57,15 @@ private:
   // Tempo que pseudoCamera ficará ociosa para simular uma dada frequência
   const boost::posix_time::millisec delay;
 
-  boost::mutex mutexCaptureFrame;
+  // boost::mutex mutexCaptureFrame;
+
+  IplImage* currentFrame;
+  IplImage* frameBuffer[2];
+
+  boost::shared_mutex bufferAccess;
+  boost::condition_variable_any unreadFrameCondition;
+
+  boost::scoped_ptr<boost::thread> grabFrameThread;
 };
 
 #endif
